26b9c3217943be326f54741ca6ceffe6a9ec0297
Michal Sz <michal.sznurowski@gmail.com>
2023-06-18 10:07:35 +0300

    commit

---
+++ dhamma_date/dhamma_date/dhamma_dateApp.swift
@@ -0,0 +1,17 @@
+//
+//  dhamma_dateApp.swift
+//  dhamma_date
+//
+//  Created by Michal Sznurowski on 17/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct dhamma_dateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
